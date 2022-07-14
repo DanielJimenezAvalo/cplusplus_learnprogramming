a7edacb7fb998b83ecc22934eef6134cf7723088
djimeneza1 <djimeneza@uni.pe>
2022-07-13 20:30:26 -0500

    lesson47_long

--- 6_variables_and_constant/47_lesson.cpp
+++ 6_variables_and_constant/47_lesson.cpp
@@ -8,9 +8,27 @@ using namespace std;
 
 int main(){
 
-    int age {18};
+    // char type
 
-    cout<<age<<endl;
+    char middle_initial {'J'}; //Notice the single quotes around characters
+    cout<<"My middle initial is "<< middle_initial << endl;
+
+    // integer types
+
+    unsigned short int exam_score {55};
+    cout<<"My exam score was "<<exam_score<<endl;
+
+    int countries_represented {65};
+    cout<<"There were "<<countries_represented<<" countries represented in my meeting"<<endl;
+
+    long people_in_florida {20610000};
+    cout<<"there are about "<<people_in_florida<<" people in Florida"<<endl;
+
     
+
+
+
+
+
     return 0;
 }
