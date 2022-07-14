7b1e4b982de679e714def3794652576a4f4a1d9e
djimeneza1 <djimeneza@uni.pe>
2022-07-14 18:46:18 -0500

    48 lesson

--- 6_variables_and_constant/47_lesson.cpp
+++ 6_variables_and_constant/47_lesson.cpp
@@ -24,11 +24,32 @@ int main(){
     long people_in_florida {20610000};
     cout<<"there are about "<<people_in_florida<<" people in Florida"<<endl;
 
+    long long people_on_earth {7'600'000'000};
+    cout<<" there are about "<<people_on_earth<<" people on earth"<<endl;
     
+    // floatinf point types
 
+    float car_payment {401.23};
+    cout<<" my car payment is "<<car_payment<<endl;
 
+    double pi {3.141592};
+    cout<<" PI is "<<pi<<endl;
 
+    long double large_amount {2.7e120};
+    cout<<large_amount<<" is a very big number "<<endl;
 
+    // boolean
+
+    bool game_over {false};
+    cout<<" the value of game over is "<<game_over<<endl;
+
+    // overflow examples
+
+    short value1 {30000};
+    short value2 {1000};
+    short product {value1*value2};
+
+    cout<<" the product of "<<value1<<" and "<<value2<<" is "<<product<<endl;
 
     return 0;
 }
