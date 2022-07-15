c0f10c3b6c4b75ec15d0a57803213e9b4783f667
djimeneza1 <djimeneza@uni.pe>
2022-07-15 13:07:33 -0500

    arrays prueba

---
+++ 7_arrays_and_vectors/53_61_lesson.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    char vowels[] {'a','e','i','o','u'};
+
+    cout<<"\nThe first vowel is: "<<vowels[0]<<endl;
+
+    cout<<"the last vowel is: "<<vowels[4]<<endl;
+
+    return 0;
+}
