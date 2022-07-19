a2f02a2a1238a3f63e23859a1551652e6436f5d5
DanielJimenezAvalo <80922898+DanielJimenezAvalo@users.noreply.github.com>
2022-07-18 21:42:04 -0500

    statements

---
+++ 8_statements_and_operators/69_testing_of_equality.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    bool equal_result {false};
+
+    bool not_equal_result {false};
+
+    int num1 {}, num2 {};
+
+    cout<<"enter two integers separeted by a space: ";
+
+    cin>>num1>>num2;
+
+    
+
+
+}
