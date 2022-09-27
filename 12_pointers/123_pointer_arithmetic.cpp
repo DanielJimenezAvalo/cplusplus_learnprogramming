8600987cba369f2e12fe19f39c7917a4fd21dc8c
djimeneza1 <djimeneza@uni.pe>
2022-09-27 17:33:36 -0500

    comments of pointers and references

--- 12_pointers/123_pointer_arithmetic.cpp
+++ 12_pointers/123_pointer_arithmetic.cpp
@@ -1,3 +1,65 @@
+/*
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%    pointer arithmetic  %%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+& pointers can be used in
+    - assignment expressions
+    - arithmetic expresions
+    - comparision expresions
+
+& c++ allows pointer arithmetic
+
+& pointer arithmetic only makes sense with arrays
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%    ++ and --  %%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+& (++) increments a pointer to point to the next array element
+
+        int_ptr++;
+
+& (--) decrements a pointer to point to the previous array element
+
+        int_ptr--;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%    + and -  %%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+& (+) increment pointer by n*sizeof(type)
+
+        int_ptr += n    ;   or      int_ptr = int_ptr + n   ;
+
+& (-) decrement pointer by n*sizeof(type)
+
+        int_ptr -= n    ;   or      int_ptr = int_ptr - n   ;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%    substracting two pointers  %%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+& determine the number of elements between the pointers
+
+& both pointers must point to the same data type
+
+        int n = int_ptr2 - int_ptr1;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%    comparing two pointers  == and !=  %%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+& determine if two pointers point to the same location
+
+    - does NOT compare the data where they point!
+
+    - you must compare the referenced pointers
+
+*/
+
+
 #include <iostream>
 #include <string>
 #include <vector>
@@ -11,14 +73,27 @@ int main() {
 
     while(*score_ptr != -1){
         cout<<"*score_ptr: "<<*score_ptr<<endl;
-        score_ptr++;//next position of the array
+        score_ptr++;// next position of the array
+        /*
+            *score_ptr: 100
+            *score_ptr: 95
+            *score_ptr: 89
+            *score_ptr: 68
+        */
     }
 
     cout<<"\n-------------------"<<endl;
     score_ptr = scores;
     while (*score_ptr != -1){
         cout<<"*score_ptr: "<<*score_ptr++<<endl;//we use first *score_ptr and after ++
+        /*
+            *score_ptr: 100
+            *score_ptr: 95
+            *score_ptr: 89
+            *score_ptr: 68
+        */
     }
+
 
     cout<<"\n-------------------"<<endl;
     string s1 {"Frank"};
@@ -30,14 +105,14 @@ int main() {
     string *p3 {&s1};
 
     cout<<boolalpha;
-    cout<<p1<<"=="<<p2<<":"<<(p1==p2)<<endl; //false
-    cout<<p1<<"=="<<p3<<":"<<(p1==p3)<<endl; //true
+    cout<<p1<<"=="<<p2<<":"<<(p1==p2)<<endl; //     0x7fff4cf45660==0x7fff4cf45680:false
+    cout<<p1<<"=="<<p3<<":"<<(p1==p3)<<endl; //     0x7fff4cf45660==0x7fff4cf45660:true
 
-    cout<<p1<<"=="<<p2<<":"<<(*p1==*p2)<<endl; //true
-    cout<<p1<<"=="<<p3<<":"<<(*p1==*p3)<<endl; //true
+    cout<<p1<<"=="<<p2<<":"<<(*p1==*p2)<<endl; //   0x7fff4cf45660==0x7fff4cf45680:true
+    cout<<p1<<"=="<<p3<<":"<<(*p1==*p3)<<endl; //   0x7fff4cf45660==0x7fff4cf45660:true
 
     p3 = &s3; //point to james
-    cout<<p1<<"=="<<p3<<":"<<(*p1==*p3)<<endl; //false
+    cout<<p1<<"=="<<p3<<":"<<(*p1==*p3)<<endl; //   0x7fff4cf45660==0x7fff4cf456a0:false
 
     cout<<"\n-------------------"<<endl;
     char name[]{"Frank"};
@@ -49,9 +124,8 @@ int main() {
     char_ptr2 = &name[3];
 
     cout<<"in the string "<<name<<", "<<*char_ptr2<<" is "<<(char_ptr2-char_ptr1)<<"  characters aways from "<<*char_ptr1<<endl;
+    //  in the string Frank, n is 3  characters aways from F
     
-
-
     cout<<endl;
     return 0;
 
--- 12_pointers/128_whats_reference_is.cpp
+++ 12_pointers/128_whats_reference_is.cpp
@@ -1,3 +1,47 @@
+/*
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%    What is a reference %%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+& an alias for a variable
+
+& must be initialized to a variable when declared
+
+& cannot be null
+
+& once initialized cannot de made to refer to a different variable
+
+& very useful as function paramaters 
+
+& might be helpful to think of a reference as a constant pointer that is automatically dereferenced
+
+& using reference in range-based for loop
+
+                vector<string> stooges {"larry","moe","curly"};
+
+                for (auto str: stooges){
+                    str = "Funny"   //change the copy
+                }
+
+                for (auto str: stooges){
+                    cout<<str<<endl;    // larry,moe,curly
+                }
+
+& using reference in range-based for loop
+
+                vector<string> stooges {"larry","moe","curly"};
+
+                for (auto &str: stooges){
+                    str = "Funny"   //change the copy
+                }
+
+                for (auto str: stooges){
+                    cout<<str<<endl;    // Funny,Funny,Funny
+                }
+
+*/
+
+
 #include<iostream>
 #include<string>
 #include<vector>
@@ -9,28 +53,34 @@ int main(){
     int num {100};
     int &ref {num};
 
-    cout<<num<<endl;
-    cout<<ref<<endl;
+    cout<<num<<endl;    //100
+    cout<<ref<<endl;    //100
 
     num=200;
     cout<<"\n-----------------------"<<endl;
-    cout<<num<<endl;
-    cout<<ref<<endl;
+    cout<<num<<endl;    //200
+    cout<<ref<<endl;    //200
 
     ref=300;
     cout<<"\n-----------------------"<<endl;
-    cout<<num<<endl;
-    cout<<ref<<endl;
+    cout<<num<<endl;    //300   
+    cout<<ref<<endl;    //300
 
     cout<<"\n-----------------------"<<endl;
     vector<string> stooges {"curly","larry","moe"};
 
     for ( auto &str: stooges ){
-        str="Funny";
+        str="Funny";    // str is a reference to each vector element
     }
+    
+    for ( auto const &str: stooges ){   //  notice we are using const
+        cout<<str<<endl;                //  now the vector elements have changed
 
-    for ( auto const &str: stooges ){
-        cout<<str<<endl;
+    /*
+        Funny
+        Funny
+        Funny
+    */
     }
 
 
